66d0f121ccdb412799dcd12ca84599b6409df155
mi-ann <wir24001@byui.edu>
2025-01-25 21:11:53 -0300

    box try

---
+++ gift_box/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import GiftBoxAnimation from "./GiftBoxAnimation";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <GiftBoxAnimation />
+  </React.StrictMode>,
+  rootElement
+);
+
